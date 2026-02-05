e96fe21364a035e32af3454449b5ba614ec7aa53
Cole Barach <colebarach@tutanota.com>
2026-02-05 15:15:13 -0500

    General fixes for the init-system layer. Not certain it is working after this, just that it certainly wouldn't work before this.

--- src/init_system/exec-dashboard-gui.sh
+++ src/init_system/exec-dashboard-gui.sh
@@ -2,4 +2,4 @@
 
 # Note: Do not forget the 'exec' command. This command replaces the current shell instance with the process to execute, meaning
 # all received signals are forwarded to the new process.
-exec $ZRE_CANTOOLS_DIR/bin/dashboard-gui $ZRE_CANTOOLS_DIR/config/zr25_glory/dashboard_vehicle_config.json can0@1000000 $ZRE_CANTOOLS_DIR/config/zr25_glory/can_vehicle.dbc
+exec cage -- $ZRE_CANTOOLS_DIR/bin/dashboard-gui $ZRE_CANTOOLS_DIR/config/zr25_glory/dashboard_vehicle_config.json can0@1000000 $ZRE_CANTOOLS_DIR/config/zr25_glory/can_vehicle.dbc
