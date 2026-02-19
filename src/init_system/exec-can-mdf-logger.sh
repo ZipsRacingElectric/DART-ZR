68150e38aa86b0eb68017e6fb955dfa2401d53ec
Cole Barach <colebarach@tutanota.com>
2026-02-19 10:55:49 -0500

    Changed the default user to root, as the permission issues were getting on my nerves. Technically more error prone, however there really isn't much concern of bricking the OS due to the ease of reflashing it.

--- src/init_system/exec-can-mdf-logger.sh
+++ src/init_system/exec-can-mdf-logger.sh
@@ -2,4 +2,6 @@
 
 # Note: Do not forget the 'exec' command. This command replaces the current shell instance with the process to execute, meaning
 # all received signals are forwarded to the new process.
-exec $ZRE_CANTOOLS_DIR/bin/can-mdf-logger /home/zre/mdf/ $ZRE_CANTOOLS_DIR/config/zr25_glory/logger_vehicle_config.json can0@1000000
+# TODO(Barach): Update to ZRE_CANTOOLS_LOGGING_DIR
+# TODO(Barach): Multiple CAN channels
+exec $ZRE_CANTOOLS_DIR/bin/can-mdf-logger /root/mdf/ $ZRE_CANTOOLS_DIR/config/zr25_glory/logger_vehicle_config.json can0@1000000
--- src/init_system/pre-exec.sh
+++ src/init_system/pre-exec.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 # Set up the can0 interface at a baudrate of 1 Mbit
-sudo ip link set up can0 type can bitrate 1000000
+ip link set up can0 type can bitrate 1000000
