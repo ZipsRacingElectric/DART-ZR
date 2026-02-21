909669715f00d66e8bf96f08a5e04007701f225b
Cole Barach <colebarach@tutanota.com>
2026-02-21 11:19:51 -0500

    Updated to latest ZRE-CAN-Tools versions and started cleaning up the filesystem. Data logs are now written to the ZRE_CANTOOLS_LOGGING_DIR directory. Added a placeholder for how different vehicle configurations are to be managed.

--- src/init_system/exec-can-mdf-logger.sh
+++ src/init_system/exec-can-mdf-logger.sh
@@ -2,6 +2,5 @@
 
 # Note: Do not forget the 'exec' command. This command replaces the current shell instance with the process to execute, meaning
 # all received signals are forwarded to the new process.
-# TODO(Barach): Update to ZRE_CANTOOLS_LOGGING_DIR
 # TODO(Barach): Multiple CAN channels
-exec $ZRE_CANTOOLS_DIR/bin/can-mdf-logger /root/mdf/ $ZRE_CANTOOLS_DIR/config/zr25_glory/logger_vehicle_config.json can0@1000000
+exec $ZRE_CANTOOLS_DIR/bin/can-mdf-logger $ZRE_CANTOOLS_LOGGING_DIR $ZRE_CANTOOLS_DIR/config/zr25/vehicle/can_mdf_logger.json can0@1000000
