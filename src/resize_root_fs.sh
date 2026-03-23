e18b686562a95b852dfdc792ed3f9f14b70e1547
Cole Barach <colebarach@tutanota.com>
2026-03-23 18:00:15 -0400

    Second attempt at resize the root filesystem. Found an example online this is based off of, so hopefully will go more smoothly.

---
+++ src/resize_root_fs.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+# Referenced from: https://github.com/jonnymacs/rpi-auto-resize-root
+
+if [ ! -f /root/resize_root_fs_done ]; then
+	raspi-config --expand-rootfs
+	touch /root/resize_root_fs_done
+	sync
+	# reboot now
+fi
